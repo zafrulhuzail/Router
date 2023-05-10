8503c6fbce0f3f2847d4325b4721d07220fdbcd6
Zafrul Huzail <huzailakazaf@gmail.com>
2023-05-10 16:16:30 +0200

    added http scheme

--- app.py
+++ app.py
@@ -7,24 +7,11 @@ controllers = {} #dict
 
 @app.route('/api/controllers/<controller_id:str>/rpc/<sub_path:path>')
 def rpc_request(controller_id: str, sub_path: str):
-    """
-    send http request to controller
-
-    1. get address of controller with id controllrt_id
-    2. send request to said controller address containing
-        - incoming request method
-        - incoming headers
-        - incoming subpath
-        - incoming payload
-    3. return controller response containing
-        - response status code 
-        - response data
-        - response headers
-    """
+ 
     adress = controllers[controller_id]['address']
     response = requests.request(
         method=flask.request.method, 
-        url=f'{adress}/{sub_path}',
+        url=f'http://{adress}/{sub_path}',
         headers=flask.request.headers, 
         data=flask.request.data    
     )
