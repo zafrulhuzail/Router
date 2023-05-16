11b218de50f81ef89ad8b02e8b7d2dfc16e28d7b
huzailakazaf <huzailakazaf@gmail.com>
2023-05-16 22:11:11 +0200

    tested endpoint on postman

--- app.py
+++ app.py
@@ -5,36 +5,46 @@ app = flask.Flask(__name__)
 
 controllers = {} #dict
 
-@app.route('/api/controllers/<controller_id:str>/rpc/<sub_path:path>')
+@app.route('/api/controllers/<controller_id>/rpc/<path:sub_path>', methods = ['GET', 'POST'])
 def rpc_request(controller_id: str, sub_path: str):
- 
-    adress = controllers[controller_id]['address']
-    response = requests.request(
-        method=flask.request.method, 
-        url=f'http://{adress}/{sub_path}',
-        headers=flask.request.headers, 
-        data=flask.request.data    
-    )
+    
+    if(controller_id in controllers.keys()):
+        adress = controllers[controller_id]['address']
+        response = requests.request(
+            method=flask.request.method, 
+            url=f'http://{adress}/{sub_path}',
+            headers=flask.request.headers, 
+            data=flask.request.data    
+        )
+
+    else: 
+     return 'Invalid id'
 
     status = response.status_code
     data = response.content
-    headers = response.headers
-   
+    # headers = response.headers
+    headers = dict(response.headers)
+
     return flask.Response(
         status=status,
-        data=data,
+        response=data,
         headers=headers               
     )
-
+    
 @app.post('/api/register')
 def register_controller():  
     data = flask.request.json 
     #store ip adress into data
-    data['adress'] = flask.request.remote_addr 
+    data['address'] = flask.request.remote_addr 
     controllers[data['id']] = data
+    # print(controllers)
+    return 'Controller registered successfully'
 
 @app.get('/api/controllers')
 def get_list_controllers():  
     return {
-        "controllers": controllers.values()  #refer postman
+        "controllers": list(controllers.values())  #refer postman
     }
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port = 8082)
