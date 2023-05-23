89f64bbe0335a606514edb8308b5ca470479e5a0
Zafrul Huzail <huzailakazaf@gmail.com>
2023-05-23 12:49:00 +0200

    updated version

--- app.py
+++ app.py
@@ -8,21 +8,20 @@ controllers = {} #dict
 @app.route('/api/controllers/<controller_id>/rpc/<path:sub_path>', methods = ['GET', 'POST'])
 def rpc_request(controller_id: str, sub_path: str):
     
-    if(controller_id in controllers.keys()):
-        adress = controllers[controller_id]['address']
-        response = requests.request(
-            method=flask.request.method, 
-            url=f'http://{adress}/{sub_path}',
-            headers=flask.request.headers, 
-            data=flask.request.data    
-        )
+    if controller_id not in controllers.keys():
+        return 'Invalid id', 404
 
-    else: 
-     return 'Invalid id'
+     
+    adress = controllers[controller_id]['address']
+    response = requests.request(
+        method=flask.request.method, 
+        url=f'http://{adress}/{sub_path}',
+        headers=flask.request.headers, 
+        data=flask.request.data    
+    )
 
     status = response.status_code
     data = response.content
-    # headers = response.headers
     headers = dict(response.headers)
 
     return flask.Response(
@@ -38,13 +37,10 @@ def register_controller():
     data['address'] = flask.request.remote_addr 
     controllers[data['id']] = data
     # print(controllers)
-    return 'Controller registered successfully'
+    return 'Controller registered successfully', 201
 
 @app.get('/api/controllers')
 def get_list_controllers():  
     return {
         "controllers": list(controllers.values())  #refer postman
     }
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port = 8082)
